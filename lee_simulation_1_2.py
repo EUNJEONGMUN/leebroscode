54c3b33a51084d8537f838fe884ea8879883cd41
EUNJEONGMUN <munej26@gmail.com>
2021-02-03 22:40:21 +0900

    try_1

--- lee_simulation_1_2.py
+++ lee_simulation_1_2.py
@@ -1,26 +1,45 @@
 n,m = map(int,input().split())
 grid = [list(map(int,input().split()))for i in range(n)]
 answer = 0
+
+
+
 for i in range(n):
     count = 1
-    for j in range(n-1):
-        if grid[i][j] == grid[i][j+1]:
+    mark = 0
+    if n==1:
+        answer += 1
+        break
+    for j in range(1,n):
+
+
+        if grid[i][j] == grid[i][j-1]:
             count+=1
         else:
-            count = 1
-    if count >= m:
+            if mark == 0 and count >= m:
+                mark = 1
+                answer += 1
+    if mark == 0 and count >= m:
         answer += 1
+
+
         
         
 for j in range(n):
     count = 1
-    for i in range(n-1):
-        if grid[i][j] == grid[i+1][j]:
+    mark = 0
+    if n == 1:
+        answer += 1
+        break
+    for i in range(1,n):
+        if grid[i][j] == grid[i-1][j]:
             count+=1
         else:
-            count = 1
-    if count >= m:
+            if mark == 0 and count>=m:
+                mark = 1
+                answer += 1
+    if mark == 0 and count >= m:
         answer += 1
 
 
-print(answer)
+print(answer) 
